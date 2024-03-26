0619e5b69c3943c195b5d0a5933276af3808465c
Bragakaii <bragaluciano.nan@gmail.com>
2024-03-26 12:24:22 -0300

    ai que nn sei oq, nn sei oq la

---
+++ js/home.js
@@ -0,0 +1,8 @@
+const search = document.getElementById('search').value
+const searchButton = document.getElementById('search-button')
+
+const searching = function(){
+    alert('FUNCIONOU O BOTÃO')
+}
+
+searchButton.addEventListener('click', searching)
